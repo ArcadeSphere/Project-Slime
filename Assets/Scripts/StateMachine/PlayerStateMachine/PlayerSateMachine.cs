f65838625090a06c73906e1b4710053f25dea657
F0XShabong <153483080+F0XShabong@users.noreply.github.com>
2024-01-23 13:27:09 +0530

    added incomplete statemachine

---
+++ Assets/Scripts/StateMachine/PlayerStateMachine/PlayerSateMachine.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSateMachine : MonoBehaviour
+{
+   public PlayerState CurrentState { get; private set; }
+    
+}
---
+++ Assets/Scripts/StateMachine/PlayerStateMachine/PlayerState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected Player player;
+    protected PlayerSateMachine stateMachine;
+    protected PlayerCore playerCore;
+    protected float startTime;
+    private string animBoolName;
+
+    public PlayerState(Player player, PlayerSateMachine stateMachine ,PlayerCore playerCore, string animBoolName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.playerCore = playerCore;
+        this.animBoolName = animBoolName;
+    }
+
+    public virtual void PlayerEnterState()
+    {
+        CheckForSomething();
+        startTime = Time.time;
+    }
+    public virtual void PLayerExitState()
+    {
+
+    }
+    public virtual void PLayerLogic()
+    {
+
+    }
+    public virtual void PLayerPhysics()
+    {
+        CheckForSomething();
+    }
+
+    public virtual void CheckForSomething()
+    {
+
+    }
+}
